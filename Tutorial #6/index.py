2e7fe912f3a2f76b3bbe48eb4ddeeb65537de6a8
Jose Daniel Castañeda Urdinola <jdcastaneu@eafit.edu.co>
2025-09-30 21:32:09 -0500

    TUTORIAL #6 UPDATE

--- Tutorial #6/index.py
+++ Tutorial #6/index.py
@@ -14,13 +14,11 @@ phrases = [
     "Alone or not you gonna walk forward.",
 ]
 
-
 @app.route('/')
 def get_random_quote():
     phrase = random.choice(phrases)
     container_id = os.uname()[1]
     return f"{phrase} - Container Id: {container_id}"
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
